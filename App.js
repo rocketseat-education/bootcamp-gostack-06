8b2dbc16ad03097b4fb5c6079b38c3f062008eef
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-30 11:28:26 -0300

    04 - ESLint, Prettier & EditorConfig

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.welcome}>Abaixo</Text>
+    </View>
+  );
+}
